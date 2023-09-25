7cf44b4c169d6c9a0ade680240107ea1e63566d9
Tony Beta Lambda <tonybetalambda@gmail.com>
2023-09-25 20:33:07 +0800

    Sketch of proof of structure test

--- Mathlib/ModelTheory/QuantifierElimination.lean
+++ Mathlib/ModelTheory/QuantifierElimination.lean
@@ -7,23 +7,7 @@ namespace FirstOrder
 
 namespace Language
 
-variable {L : Language.{u, v}} {T : L.Theory} {α : Type w} {n : ℕ}
-
-namespace Theory
-
-variable (T)
-
-def eliminatesQuantifier (φ : L.BoundedFormula α n) : Prop :=
-  ∃ ψ, ψ.IsQF ∧ T.SemanticallyEquivalent φ ψ
-
-def hasQE : Prop :=
-  ∀ {α : Type w} (φ : L.BoundedFormula α n), T.eliminatesQuantifier φ
-
-end Theory
-
-namespace Embedding
-
-variable (hQE : T.hasQE)
+variable {L : Language.{u, v}} {T : L.Theory} {α : Type w}
 
 variable (M : Type (max u v)) (N : Type (max u v))
 
@@ -31,11 +15,47 @@ variable [Nonempty M] [Nonempty N]
 variable [L.Structure M] [L.Structure N]
 variable (_hM : T.Model M) (_hN : T.Model N)
 
+namespace Theory
+
+def eliminatesQuantifier (φ : L.Formula α) : Prop :=
+  ∃ ψ, ψ.IsQF ∧ T.SemanticallyEquivalent φ ψ
+
+theorem eliminatesQuantifier_iff_realize_common_substructure {φ : L.Formula α} :
+    T.eliminatesQuantifier φ ↔
+    (∀ {A} {M N : Type max u v} [L.Structure A] [Nonempty M] [Nonempty N]
+      [L.Structure M] [L.Structure N] [T.Model M] [T.Model N]
+      (f : A ↪[L] M) (g : A ↪[L] N) (v : α → A),
+      φ.Realize (f ∘ v) ↔ φ.Realize (g ∘ v)) := by
+  constructor
+  · intro ⟨ψ, hQF, h⟩ _ M N _ _ _ _ _ _hM _hN f g v
+    rw [h.realize_iff _hM, h.realize_iff _hN, f.realize_QF_formula hQF, g.realize_QF_formula hQF]
+  · intro h
+    let ι := L.lhomWithConstants α
+    let T₁ := (ι.onTheory T).insert (Formula.equivSentence φ)
+    -- S is the set of quantifier free consequences of φ
+    let S := { σ : L[[α]].Sentence | σ.IsQF ∧ T₁ ⊨ᵇ σ }
+    -- we show that T' := T ∪ S ∪ {¬φ} is not satisfiable
+    let T' : L[[α]].Theory := (ι.onTheory T) ∪ S
+    have : ¬ Theory.IsSatisfiable (T' ∪ {∼(Formula.equivSentence φ)}) := sorry
+    obtain ⟨T₂, h', h⟩ := models_iff_finset_models.mp ((models_iff_not_satisfiable _).mpr this)
+    let ψ := BoundedFormula.iInf T₂ Formula.equivSentence.invFun
+    use ψ; constructor
+    · apply BoundedFormula.isQF_iInf
+      sorry
+    · sorry
+
+def hasQE : Prop :=
+  ∀ {α : Type w} (φ : L.Formula α), T.eliminatesQuantifier φ
+
+end Theory
+
+namespace Embedding
+
+variable (hQE : T.hasQE)
+
 theorem isElementry_of_hasQE (f : M ↪[L] N) :
     ∀ {n} (φ : L.Formula (Fin n)) (v : Fin n → M), φ.Realize (f ∘ v) ↔ φ.Realize v := by
   intro n φ v
   obtain ⟨ψ, hQF, h⟩ := hQE φ
-  rw [h.realize_iff _hM, h.realize_iff _hN, Formula.Realize]
-  have : default = f ∘ (default : Fin 0 → M) := by simp
-  rw [this]
-  apply f.realize_QFFormula hQF
+  rw [h.realize_iff _hM, h.realize_iff _hN]
+  exact f.realize_QF_formula hQF
